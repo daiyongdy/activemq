3d9996305fc29aaf617e1082c35d65d73886467a
daiyong <daiyong@coohua.com>
2017-07-10 18:57:31 +0800

    update

--- mq-producer/src/main/java/com/daiyong/mq/QueueReceiver.java
+++ mq-producer/src/main/java/com/daiyong/mq/QueueReceiver.java
@@ -1,12 +1,37 @@
 package com.daiyong.mq;
 
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
 /**
  * Created by daiyong on 2017/7/10.
  * email daiyong@coohua.com
  */
 public class QueueReceiver {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://192.168.100.131:61616");
 
+		Connection connection = connectionFactory.createConnection();
+		connection.start();
+
+		Session session = connection.createSession(true, Session.AUTO_ACKNOWLEDGE);
+
+		Destination destination = session.createQueue("my-queue");
+
+		MessageConsumer messageConsumer = session.createConsumer(destination);
+
+		int i = 0 ;
+
+		while (i < 3) {
+			i ++;
+			TextMessage textMessage = (TextMessage) messageConsumer.receive();
+			session.commit();
+			System.out.println(textMessage.getText());
+		}
+
+		session.close();
+		connection.close();
 	}
 }
